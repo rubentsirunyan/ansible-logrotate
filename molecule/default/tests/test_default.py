1a12c0d71ff23649003cf4278722cfdc544c8867
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-05-24 15:39:56 +0400

    Updating tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,16 @@ def test_apache_logrotate_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_default_cron_daily_file_absence(host):
+    f = host.file('/etc/cron.daily/logrotate')
+
+    assert not f.exists
+
+
+def test_line_exists_in_crontab(host):
+    f = host.file('/etc/crontab')
+
+    assert f.exists
+    assert f.contains("/usr/sbin/logrotate -s")
